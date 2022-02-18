820b38b442d6c981c7ed4c6dbc46724be7d56e71
jason-smith-code <jasonsmithcode@gmail.com>
2022-02-18 15:39:57 +0000

    transferred javascript to eternal file, finished product grid, began a full width feature grid, altered some css for better styling

--- inventory.js
+++ inventory.js
@@ -1,27 +1,472 @@
 const inventory = [
     {
-        title: "titanium white",
-        id: 1,
+        title: "Titanium White",
+        image: "images/products/116.jpg",
+        opacity: 3,
+        lightfast: 3,
+        code: "116"
+    },
+    {
+        title: "Iridescent Pearl",
         image: "images/products/020.jpg",
         opacity: 1,
-        lightfast: 2,
-        size: [small, large],
+        lightfast: 3,
         code: "020"
     },
     {
-        title: "buff titanium",
-        id: 1,
-        image: "images/products/028.jpg",
+        title: "Iridescent Silver",
+        image: "images/products/029.jpg",
+        opacity: 1,
+        lightfast: 3,
+        code: "029"
+    },
+    {
+        title: "Blush Tint",
+        image: "images/products/650.jpg",
         opacity: 2,
         lightfast: 3,
-        size: [small, large],
-        code: "040"
+        code: "650"
+    },
+    {
+        title: "Buff Titanium",
+        image: "images/products/136.jpg",
+        opacity: 3,
+        lightfast: 3,
+        code: "136"
+    },
+    {
+        title: "Flesh Ochre",
+        image: "images/products/250.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "250"
+    },
+    {
+        title: "Fluorescent Yellow",
+        image: "images/products/502.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "502"
+    },
+    {
+        title: "Cadmium Yellow Medium Hue",
+        image: "images/products/541.jpg",
+        opacity: 1,
+        lightfast: 3,
+        code: "541"
+    },
+    {
+        title: "Cadmium Yellow Deep Hue",
+        image: "images/products/543.jpg",
+        opacity: 1,
+        lightfast: 3,
+        code: "543"
+    },
+    {
+        title: "Cadmium Yellow Lemon Hue",
+        image: "images/products/545.jpg",
+        opacity: 1,
+        lightfast: 3,
+        code: "545"
+    },
+    {
+        title: "Dark Naples Yellow",
+        image: "images/products/566.jpg",
+        opacity: 1,
+        lightfast: 3,
+        code: "566"
+    },
+    {
+        title: "Naples Yellow",
+        image: "images/products/567.jpg",
+        opacity: 1,
+        lightfast: 3,
+        code: "567"
+    },
+    {
+        title: "Primary Yellow",
+        image: "images/products/574.jpg",
+        opacity: 1,
+        lightfast: 3,
+        code: "574"
+    },
+    {
+        title: "Warm Grey",
+        image: "images/products/705.jpg",
+        opacity: 3,
+        lightfast: 3,
+        code: "705"
+    },
+    {
+        title: "Red Orange",
+        image: "images/products/640.jpg",
+        opacity: 1,
+        lightfast: 3,
+        code: "640"
+    },
+    {
+        title: "Iridescent Bronze",
+        image: "images/products/022.jpg",
+        opacity: 1,
+        lightfast: 3,
+        code: "022"
+    },
+    {
+        title: "Iridescent Gold",
+        image: "images/products/028.jpg",
+        opacity: 1,
+        lightfast: 3,
+        code: "028"
+    },
+    {
+        title: "Yellow Ochre",
+        image: "images/products/252.jpg",
+        opacity: 3,
+        lightfast: 3,
+        code: "252"
+    },
+    {
+        title: "Raw Sienna",
+        image: "images/products/208.jpg",
+        opacity: 3,
+        lightfast: 3,
+        code: "208"
+    },
+    {
+        title: "Iridescent Copper",
+        image: "images/products/036.jpg",
+        opacity: 1,
+        lightfast: 3,
+        code: "036"
+    },
+    {
+        title: "Fluorescent Orange",
+        image: "images/products/648.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "648"
+    },
+    {
+        title: "Cadmium Red Orange Hue",
+        image: "images/products/615.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "615"
+    },
+    {
+        title: "Burnt Sienna",
+        image: "images/products/211.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "211"
+    },
+    {
+        title: "Burnt Umber",
+        image: "images/products/202.jpg",
+        opacity: 3,
+        lightfast: 3,
+        code: "202"
+    },
+    {
+        title: "Cadmium Red Deep Hue",
+        image: "images/products/606.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "606"
+    },
+    {
+        title: "Cadmium Red Light Hue",
+        image: "images/products/613.jpg",
+        opacity: 1,
+        lightfast: 3,
+        code: "613"
+    },
+    {
+        title: "Light English Red",
+        image: "images/products/627.jpg",
+        opacity: 1,
+        lightfast: 3,
+        code: "627"
+    },
+    {
+        title: "Carmine Red",
+        image: "images/products/635.jpg",
+        opacity: 1,
+        lightfast: 3,
+        code: "635"
+    },
+    {
+        title: "Chinese Orange",
+        image: "images/products/645.jpg",
+        opacity: 1,
+        lightfast: 3,
+        code: "645"
+    },
+    {
+        title: "Caput Mortuum",
+        image: "images/products/919.jpg",
+        opacity: 1,
+        lightfast: 3,
+        code: "919"
+    },
+    {
+        title: "Venetian Pink",
+        image: "images/products/651.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "651"
+    },
+    {
+        title: "Fluorescent Pink",
+        image: "images/products/654.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "654"
+    },
+    {
+        title: "Quinacridrone Pink",
+        image: "images/products/658.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "658"
+    },
+    {
+        title: "Deep Magenta",
+        image: "images/products/671.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "671"
+    },
+    {
+        title: "Vermillion",
+        image: "images/products/674.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "674"
+    },
+    {
+        title: "Primary Red",
+        image: "images/products/686.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "686"
+    },
+    {
+        title: "Light Violet",
+        image: "images/products/921.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "921"
+    },
+    {
+        title: "Purple",
+        image: "images/products/917.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "917"
+    },
+    {
+        title: "Coblat Blue Hue",
+        image: "images/products/303.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "303"
+    },
+    {
+        title: "Indigo Blue",
+        image: "images/products/308.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "308"
+    },
+    {
+        title: "Ultramarine Blue",
+        image: "images/products/314.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "314"
+    },
+    {
+        title: "Azure Blue",
+        image: "images/products/320.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "320"
+    },
+    {
+        title: "Ceruleum Blue Hue",
+        image: "images/products/323.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "323"
+    },
+    {
+        title: "Turquoise",
+        image: "images/products/341.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "341"
+    },
+    {
+        title: "Chinese Blue",
+        image: "images/products/346.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "346"
+    },
+    {
+        title: "Primary Blue",
+        image: "images/products/385.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "385"
+    },
+    {
+        title: "Phthalo Green",
+        image: "images/products/896.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "896"
+    },
+    {
+        title: "Hookers Green",
+        image: "images/products/809.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "809"
+    },
+    {
+        title: "Permanent Green Light",
+        image: "images/products/811.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "811"
+    },
+    {
+        title: "Light Olive Green",
+        image: "images/products/812.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "812"
+    },
+    {
+        title: "Sap Green",
+        image: "images/products/819.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "819"
+    },
+    {
+        title: "Cobalt Green Deep Hue",
+        image: "images/products/835.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "835"
+    },
+    {
+        title: "Bright Yellow Green",
+        image: "images/products/871.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "871"
+    },
+    {
+        title: "Fluorescent Green",
+        image: "images/products/895.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "895"
+    },
+    {
+        title: "Burnt Green Earth",
+        image: "images/products/214.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "214"
+    },
+    {
+        title: "Raw Umber",
+        image: "images/products/205.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "205"
+    },
+    {
+        title: "Paynes Grey",
+        image: "images/products/703.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "703"
+    },
+    {
+        title: "Iridescent Black",
+        image: "images/products/070.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "070"
+    },
+    {
+        title: "Mars Black",
+        image: "images/products/759.jpg",
+        opacity: 2,
+        lightfast: 3,
+        code: "759"
     },
 ]
 
 
 const printinventory = () => {
-    inventory.map(item => {
-        console.log(item.title)
+    var listItems = inventory.map(function(item){
+
+        const displayOpacity = () => {
+            if (item.opacity == 1) {
+                return '<div class="opacity-row"><div class="square transparent"></div></div>'
+            } else if (item.opacity == 2) {
+                return '<div class="opacity-row"><div class="square semi-transparent"></div></div>'
+            } else if (item.opacity == 3) {
+                return '<div class="opacity-row"><div class="square opaque"></div></div>'
+            } else if (item.opacity < 1 || item.opacity > 3 ) {
+                return '<p>Incorrect opacity value</p>'  
+            }
+        }
+
+        const displayLightfastness = () => {
+            if (item.lightfast == 1) {
+                return  '<div class="circle-container">' +
+                            '<div class="circle opaque"></div>' +
+                            '<div class="circle"></div>'+
+                            '<div class="circle"></div>'+
+                        '</div>'
+            } else if (item.lightfast == 2) {
+                return  '<div class="circle-container">' +
+                            '<div class="circle opaque"></div>' +
+                            '<div class="circle opaque"></div>'+
+                            '<div class="circle"></div>'+
+                        '</div>'
+            } else if (item.lightfast == 3) {
+                return  '<div class="circle-container">' +
+                            '<div class="circle opaque"></div>' +
+                            '<div class="circle opaque"></div>'+
+                            '<div class="circle opaque"></div>'+
+                        '</div>'
+            } else if (item.lightfast < 1 || item.lightfast > 3 ) {
+                return '<p>Incorrect opacity value</p>'  
+            }
+        }
+
+        return  '<div class="item">' +
+                    '<div class="image-container">' +
+                        '<img class="product-image" src=' + item.image +'>'+
+                    '</div>'+
+                    '<h3>'  + item.title + '</h3>' +
+                    '<p class="product-title">[' + item.code + ']</p>' +
+                    '<div class="product-row">' +
+                        displayOpacity() + 
+                        displayLightfastness() +
+                    '</div>'+
+                '</div>';
     })
+   document.querySelector('#app').innerHTML = listItems.join('');
 }
+
+printinventory();
